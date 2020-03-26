519887ba8aaf147f957493624c4902ac86f87040
odeyaMolodic <57504045+odeyaMolodic@users.noreply.github.com>
2020-03-26 17:18:53 +0200

    add empty find function

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,9 @@
+#include "PhoneticFinder.hpp"
+using namespace phonetic;
+
+namespace phonetic {
+    string find (string text, string s){
+
+        return "";
+    }
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,4 @@
+
+namespace phonetic {
+    string find (string text, string s);
+};
